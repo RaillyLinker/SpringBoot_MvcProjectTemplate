348a6eb7d58fa871f87775900bd26373eaeac413
RaillyLinker <raillylinker@gmail.com>
2023-09-09 20:46:00 +0900

    스케쥴러 설정

--- src/main/kotlin/com/railly_linker/springboot_mvc_project_template/ApplicationMain.kt
+++ src/main/kotlin/com/railly_linker/springboot_mvc_project_template/ApplicationMain.kt
@@ -2,7 +2,11 @@ package com.railly_linker.springboot_mvc_project_template
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableAsync
+import org.springframework.scheduling.annotation.EnableScheduling
 
+@EnableScheduling // 스케쥴러 사용 설정
+@EnableAsync // 스케쥴러의 Async 사용 설정
 @SpringBootApplication
 class ApplicationMain
 
