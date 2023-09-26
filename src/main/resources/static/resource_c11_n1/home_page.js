2189eddcb839065d0b5a1cd8dfa6a93178226abf
RaillyLinker <raillylinker@gmail.com>
2023-09-26 11:28:25 +0900

    -

--- src/main/resources/static/resource_c11_n1/home_page.js
+++ src/main/resources/static/resource_c11_n1/home_page.js
@@ -2,11 +2,4 @@ window.onload = function(){
 	//실행될 코드
 	console.log("ready!!");
 
-	document.getElementById("title_icon").onclick = function() {
-	    location.href='/';
-	};
-
-	document.getElementById("go_to_swagger_btn").onclick = function() {
-	    location.href='/swagger-ui/index.html';
-	};
 }
