cbe3f4948d9164b4f34405b61383bb68bbe15d2e
RaillyLinker <raillylinker@gmail.com>
2023-12-18 09:44:51 +0900

    -

--- src/main/kotlin/com/railly_linker/springboot_mvc_project_template/ApplicationMain.kt
+++ src/main/kotlin/com/railly_linker/springboot_mvc_project_template/ApplicationMain.kt
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.annotation.EnableScheduling
+import java.util.*
 
 @EnableScheduling // 스케쥴러 사용 설정
 @EnableAsync // 스케쥴러의 Async 사용 설정
@@ -11,5 +12,6 @@ import org.springframework.scheduling.annotation.EnableScheduling
 class ApplicationMain
 
 fun main(args: Array<String>) {
+    TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"))
     runApplication<ApplicationMain>(*args)
 }
