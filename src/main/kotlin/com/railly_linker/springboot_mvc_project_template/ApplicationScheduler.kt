b5386ad1164bce7d7c8357799853edebd458706f
RaillyLinker <raillylinker@gmail.com>
2023-12-22 20:31:27 +0900

    -

--- src/main/kotlin/com/railly_linker/springboot_mvc_project_template/ApplicationScheduler.kt
+++ src/main/kotlin/com/railly_linker/springboot_mvc_project_template/ApplicationScheduler.kt
@@ -1,6 +1,7 @@
 package com.railly_linker.springboot_mvc_project_template
 
 import org.springframework.scheduling.annotation.EnableAsync
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
 // 일정 시간마다 실행되는 함수들의 클래스 (프로젝트 Application 클래스 선언 위에 @EnableScheduling 추가 필요)
@@ -12,15 +13,15 @@ class ApplicationScheduler {
     // [사용 예시]
     // (fixedDelay)
     // 해당 메서드가 끝나는 시간 기준으로 milliseconds 후의 간격으로 실행
-//    @Scheduled(fixedDelay = 1000, zone = "Asia/Seoul")
-//    // @Scheduled(fixedDelayString = "${fixedDelay.in.milliseconds}", zone = "Asia/Seoul") // 문자열 milliseconds 사용 시
+//    @Scheduled(fixedDelay = 1000)
+//    // @Scheduled(fixedDelayString = "${fixedDelay.in.milliseconds}") // 문자열 milliseconds 사용 시
 //    fun scheduleFixedDelayTask() {
-//
+//        println("scheduleFixedDelayTask")
 //    }
 
     // (initialDelay + fixedDelay)
     // initialDelay 값 이후 처음 실행 되고, fixedDelay 값에 따라 계속 실행 = fixedDelay 에 최초 실행 시간이 달린 것
-//    @Scheduled(initialDelay = 5000, fixedDelay = 1000, zone = "Asia/Seoul")
+//    @Scheduled(initialDelay = 5000, fixedDelay = 1000)
 //    fun scheduleFixedRateWithInitialDelayTask() {
 //
 //    }
@@ -29,7 +30,8 @@ class ApplicationScheduler {
     // 해당 메서드가 시작하는 시간 기준, milliseconds 간격으로 실행
     // 병렬로 Scheduler 를 사용할 경우, Class에 @EnableAsync, Method에 @Async 추가
 //    @Async
-//    @Scheduled(fixedRate = 1000, zone = "Asia/Seoul") // @Scheduled(fixedRateString = "${fixedRate.in.milliseconds}", zone = "Asia/Seoul")  // 문자열 milliseconds 사용 시
+//    @Scheduled(fixedRate = 1000)
+//    @Scheduled(fixedRateString = "${fixedRate.in.milliseconds}")  // 문자열 milliseconds 사용 시
 //    fun scheduleFixedRateTask() {
 //
 //    }
